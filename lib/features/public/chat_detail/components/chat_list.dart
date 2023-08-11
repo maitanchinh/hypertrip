fe36899004a25fa442ad0391aae95720256231d7
DoKhaiTrinh <trinhdo.khai98@gmail.com>
2023-08-11 11:25:32 +0700

    FIx

--- lib/features/public/chat_detail/components/chat_list.dart
+++ lib/features/public/chat_detail/components/chat_list.dart
@@ -7,6 +7,7 @@ import 'package:hypertrip/domain/repositories/user_repo.dart';
 import 'package:hypertrip/features/public/chat_detail/interactor/chat_detail_bloc.dart';
 import 'package:hypertrip/theme/color.dart';
 import 'package:hypertrip/utils/app_assets.dart';
+import 'package:hypertrip/utils/app_style.dart';
 import 'package:hypertrip/utils/page_states.dart';
 
 class ChatList extends StatefulWidget {
@@ -69,8 +70,7 @@ class _ChatListState extends State<ChatList> {
                   chatBackgroundConfig: const ChatBackgroundConfiguration(
                     messageTimeIconColor: Colors.white,
                     messageTimeTextStyle: TextStyle(color: Colors.white),
-                    defaultGroupSeparatorConfig:
-                        DefaultGroupSeparatorConfiguration(
+                    defaultGroupSeparatorConfig: DefaultGroupSeparatorConfiguration(
                       textStyle: TextStyle(
                         color: Colors.black,
                         fontSize: 17,
@@ -98,7 +98,63 @@ class _ChatListState extends State<ChatList> {
                       textStyle: TextStyle(color: Colors.white),
                     ),
                   ),
-                  messageConfig: const MessageConfiguration(imageMessageConfig: ImageMessageConfiguration(width: 250,height: 250)),
+                  messageConfig: MessageConfiguration(
+                    imageMessageConfig: const ImageMessageConfiguration(width: 250, height: 250),
+                    customMessageBuilder: (message) {
+                      String urlParse = '';
+                      String textContent = '';
+                      int httpIndex = message.message.indexOf("http");
+                      if (httpIndex != -1) {
+                        textContent = message.message.substring(0, httpIndex);
+                        urlParse = message.message.substring(httpIndex);
+                      } else {
+                        urlParse = message.message; // Phần trước "http"
+                      }
+                      return Container(
+                        constraints: const BoxConstraints(minHeight: 146, maxHeight: 200),
+                        margin: const EdgeInsets.symmetric(horizontal: 16),
+                        padding: const EdgeInsets.all(16),
+                        decoration: const BoxDecoration(
+                          color: Colors.black45,
+                          borderRadius: BorderRadius.all(Radius.circular(16)),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                const Icon(Icons.telegram, size: 48, color: Colors.blueAccent),
+                                SizedBox(
+                                  width: MediaQuery.sizeOf(context).width - 159,
+                                  child: RichText(
+                                    text: TextSpan(children: [
+                                      TextSpan(
+                                          text: "Live location \n",
+                                          style: AppStyle.fontOpenSanBold.copyWith(fontSize: 22)),
+                                      TextSpan(
+                                          text: textContent,
+                                          style: AppStyle.fontOpenSanBold
+                                              .copyWith(fontSize: 16, color: AppColors.textColor)),
+                                    ]),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            MaterialButton(
+                              height: 56,
+                              color: Colors.grey,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(16)),
+                                side: BorderSide.none,
+                              ),
+                              onPressed: () {},
+                              child: Text('View location',style: AppStyle.fontOpenSanRegular.copyWith(color: Colors.white)),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                   sendMessageConfig: SendMessageConfiguration(
                     sendButtonIcon: SvgPicture.asset(
                       AppAssets.icons_paper_plane_svg,
@@ -110,11 +166,9 @@ class _ChatListState extends State<ChatList> {
                       cameraIconColor: AppColors.greyColor,
                       galleryIconColor: AppColors.greyColor,
                       mapIconColor: AppColors.greyColor,
-                      cameraImagePickerIcon: SvgPicture.asset(
-                          AppAssets.icons_ic_camera_svg,
+                      cameraImagePickerIcon: SvgPicture.asset(AppAssets.icons_ic_camera_svg,
                           color: AppColors.primaryColor),
-                      galleryImagePickerIcon: SvgPicture.asset(
-                          AppAssets.icons_ic_picture_svg,
+                      galleryImagePickerIcon: SvgPicture.asset(AppAssets.icons_ic_picture_svg,
                           color: AppColors.primaryColor),
                       mapIcon: SvgPicture.asset(AppAssets.icons_map_svg,
                           width: 24, height: 24, color: AppColors.primaryColor),
@@ -129,8 +183,7 @@ class _ChatListState extends State<ChatList> {
                     ),
                     allowRecordingVoice: false,
                     micIconColor: Colors.white,
-                    voiceRecordingConfiguration:
-                        const VoiceRecordingConfiguration(
+                    voiceRecordingConfiguration: const VoiceRecordingConfiguration(
                       backgroundColor: Color(0xff383152),
                       recorderIconColor: Color(0xff757575),
                       waveStyle: WaveStyle(
@@ -148,8 +201,7 @@ class _ChatListState extends State<ChatList> {
                   ),
                   replyPopupConfig: ReplyPopupConfiguration(
                     onReplyTap: (message) {},
-                    replyPopupBuilder: (message, sendByCurrentUser) =>
-                        const SizedBox(),
+                    replyPopupBuilder: (message, sendByCurrentUser) => const SizedBox(),
                   ),
                   onPressedMap: widget.onPressedMap,
                   onSendTap: (message, replyMessage, messageType) {
