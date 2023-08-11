f52e16f63b8ebf1369bf79c38aefbd2b65089dfd
DoKhaiTrinh <trinhdo.khai98@gmail.com>
2023-08-11 11:42:12 +0700

    Fix

--- lib/features/public/chat_detail/components/chat_list.dart
+++ lib/features/public/chat_detail/components/chat_list.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hypertrip/domain/models/chat/firestore_message.dart';
 import 'package:hypertrip/domain/repositories/user_repo.dart';
+import 'package:hypertrip/features/public/chat_detail/components/share_location.dart';
 import 'package:hypertrip/features/public/chat_detail/interactor/chat_detail_bloc.dart';
 import 'package:hypertrip/theme/color.dart';
 import 'package:hypertrip/utils/app_assets.dart';
@@ -100,60 +101,7 @@ class _ChatListState extends State<ChatList> {
                   ),
                   messageConfig: MessageConfiguration(
                     imageMessageConfig: const ImageMessageConfiguration(width: 250, height: 250),
-                    customMessageBuilder: (message) {
-                      String urlParse = '';
-                      String textContent = '';
-                      int httpIndex = message.message.indexOf("http");
-                      if (httpIndex != -1) {
-                        textContent = message.message.substring(0, httpIndex);
-                        urlParse = message.message.substring(httpIndex);
-                      } else {
-                        urlParse = message.message; // Phần trước "http"
-                      }
-                      return Container(
-                        constraints: const BoxConstraints(minHeight: 146, maxHeight: 200),
-                        margin: const EdgeInsets.symmetric(horizontal: 16),
-                        padding: const EdgeInsets.all(16),
-                        decoration: const BoxDecoration(
-                          color: Colors.black45,
-                          borderRadius: BorderRadius.all(Radius.circular(16)),
-                        ),
-                        child: Column(
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                const Icon(Icons.telegram, size: 48, color: Colors.blueAccent),
-                                SizedBox(
-                                  width: MediaQuery.sizeOf(context).width - 159,
-                                  child: RichText(
-                                    text: TextSpan(children: [
-                                      TextSpan(
-                                          text: "Live location \n",
-                                          style: AppStyle.fontOpenSanBold.copyWith(fontSize: 22)),
-                                      TextSpan(
-                                          text: textContent,
-                                          style: AppStyle.fontOpenSanBold
-                                              .copyWith(fontSize: 16, color: AppColors.textColor)),
-                                    ]),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            MaterialButton(
-                              height: 56,
-                              color: Colors.grey,
-                              shape: const RoundedRectangleBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(16)),
-                                side: BorderSide.none,
-                              ),
-                              onPressed: () {},
-                              child: Text('View location',style: AppStyle.fontOpenSanRegular.copyWith(color: Colors.white)),
-                            )
-                          ],
-                        ),
-                      );
-                    },
+                    customMessageBuilder: (message) => ShareLocation(message: message),
                   ),
                   sendMessageConfig: SendMessageConfiguration(
                     sendButtonIcon: SvgPicture.asset(
